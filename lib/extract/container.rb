ed44ecc10fbdf829340530fb13482f223352dbde
Amos L King <amos.l.king@gmail.com>
2017-04-13 15:09:52 -0500

    Add conditional rendering question to container
    I don't want to bypass rendering at every turn, but when I do this is a
    nice helper method to have. Later we might consider an API for
    conditional rendering.
    
    ```ruby
      render_only_with_elements
    ```
    
    Amos King @adkron <amos@binarynoggin.com>

--- lib/extract/container.rb
+++ lib/extract/container.rb
@@ -10,10 +10,18 @@ module OpenXml
       children << child
     end
 
+    def push(child)
+      children.push(child)
+    end
+
     def to_xml(xml)
       xml[namespace].public_send(tag, xml_attributes) {
         children.each { |child| child.to_xml(xml) }
       }
+    end
+
+    def render?
+      super || children.any?
     end
   end
 end
